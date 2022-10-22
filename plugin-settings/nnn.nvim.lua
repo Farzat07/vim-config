bc06f9f73245d5d9adab1b71729a7d85b4cc4ff3
A Farzat <a@farzat.xyz>
2022-10-22 12:01:23 +0900

    Add nnn.nvim plugin

---
+++ plugin-settings/nnn.nvim.lua
@@ -0,0 +1,18 @@
+local builtin = require("nnn").builtin
+require("nnn").setup({
+        explorer = {
+            cmd = "n3",       -- command overrride (-F1 flag is implied, -a flag is invalid!)
+        },
+        picker = {
+            cmd = "tmux new n3 -a -Pp",       -- command override (-p flag is implied)
+        },
+        mappings = {
+            { "<C-t>", builtin.open_in_tab },       -- open file(s) in tab
+            { "<C-s>", builtin.open_in_split },     -- open file(s) in split
+            { "<C-v>", builtin.open_in_vsplit },    -- open file(s) in vertical split
+            { "<C-p>", builtin.open_in_preview },   -- open file in preview split keeping nnn focused
+            { "<C-y>", builtin.copy_to_clipboard }, -- copy file(s) to clipboard
+            { "<C-w>", builtin.cd_to_path },        -- cd to file directory
+            { "<C-e>", builtin.populate_cmdline },  -- populate cmdline (:) with file(s)
+        },
+    })
